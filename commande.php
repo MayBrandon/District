c803bdb304a3711bce2c4ffbbbcedafe753d2d9f
mayingi Brandon <brandon.mayingi.pro@outlook.fr>
2023-09-17 13:05:45 +0200

    s

---
+++ commande.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="/assets/img/branding/favicon.ico" type="image/x-icon">
+    <title>Commande</title>
+</head>
+<body>
+    <?php include("header-footer-php/header.php") ?>
+</body>
+</html>
---
+++ header-footer-php/header.php
@@ -0,0 +1,39 @@
+
+    <header>
+    <!-- DEBUT MENU DEROULANT POUR TABLETTE ET MOBILE ET PC -->
+    <nav id="nav-bar">
+        <div class="div-logo">
+            <img class="logo-district" src="/assets/img/branding/logo-district.png" alt="">
+        </div>
+        <ul class="nav-menu">
+            <li class="li-menu">
+                <a class="lien-menu"
+                href="/../../../index.html">Accueil</a>
+            </li>
+            <li class="li-menu">
+                <a class="lien-menu"
+                href="/pages/pages-categorie/categorie.html">Catégories</a>
+            </li>
+            <li class="li-menu">
+                <a class="lien-menu"
+                href="/pages/pages-plats/plats-1.html">Plats</a>
+            </li>
+            <li class="li-menu">
+                <a class="lien-menu"
+                href="">Contact</a>
+            </li>
+            <li class="li-menu">
+                <a class="lien-menu"
+                href="/../../panier.html"><img class="cart-svg" src="/assets/img/img-plat-accueil/shopping-cart-outline-svgrepo-com.svg" alt="">
+            </a>
+            </li>
+        </ul>
+
+        <div class="hamburger">
+            <span class="bar"></span>
+            <span class="bar"></span>
+            <span class="bar"></span>
+        </div>
+    </nav>
+    <!--  FIN MENU DEROULANT POUR TABLETTE ET MOBILE -->
+    </header>
