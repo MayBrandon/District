5f558e2428f730195709918bacf5d22f0ab954be
mayingi Brandon <brandon.mayingi.pro@outlook.fr>
2023-10-10 14:12:15 +0200

    co

--- config.php
+++
@@ -1,8 +0,0 @@
-<?php 
-
-    try {
-        $bdd = new PDO("mysql:host=localhost;dbname=district;charset=utf8", "root", "");
-    }
-    catch(PDOException $e) {
-        die('Erreur : '.$e->getMessage());
-    }
